8b1355d521f4d6ccd51247b18f1a258d06e60dab
Hamid Reza <hmdrzaa11@gmail.com>
2021-01-09 22:01:27 +0330

    fixed some bugs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { HeaderWrapper } from "./Header.styles";
-import headerImage from "../../images/paul-frenzel-MnHQMzC6n-o-unsplash.jpg";
+import headerImage from "../../images/header.jpg";
 
 export default function Header() {
   return <HeaderWrapper id="home" imageUrl={headerImage}></HeaderWrapper>;
--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export let HeaderWrapper = styled.header`
   background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)),
-    url("/images/blurrystock-HIbAmybJHVs-unsplash.jpg");
+    url(${(props) => props.imageUrl});
   height: 70vh;
   background-position: center;
   background-size: cover;
