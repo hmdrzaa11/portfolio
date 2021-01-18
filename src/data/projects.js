1a6ce5d7844223d720754662b2a254336ce4dab2
Hamid Reza <hmdrzaa11@gmail.com>
2021-01-19 01:16:10 +0330

    changed data

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,14 +2,14 @@ let projects = [
   {
     name: "Portfolio",
     description: `This very website you're now watching, created with React and Styled-Components and hosted on github pages with ability of choosing dark or light theme `,
-    githubLink: "https://github.com/hmdrzaa11/maze",
+    githubLink: "https://github.com/hmdrzaa11",
     image: "portfolio.png",
     stack: ["React", "Styled-Components"],
   },
   {
     name: "Maze",
     description: `This is the Maze game which written in Vanilla js and i kept it very simple and its only in dark version`,
-    githubLink: "https://github.com/hmdrzaa11",
+    githubLink: "https://github.com/hmdrzaa11/maze",
     image: "maze.png",
     stack: ["Javascript", "Matter.js"],
   },
