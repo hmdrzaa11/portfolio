050adb36e8058dd3f97500d03ebea7b7089f95cc
Hamid Reza <hmdrzaa11@gmail.com>
2021-01-26 20:31:36 +0330

    added natours project

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,5 +13,22 @@ let projects = [
     image: "maze.png",
     stack: ["Javascript", "Matter.js"],
   },
+  {
+    name: "Movie Fight",
+    description: `This is the Movie Fight and written in Vanilla js and fetches data from an API  and compares 2 movies side by side based on the IMDB and awards that each won etc.`,
+    githubLink: "https://github.com/hmdrzaa11/movie-fight",
+    image: "movieFight.png",
+    stack: ["Javascript"],
+  },
+  {
+    name: "Natours",
+    description: `
+    This is the Movie Natours project written in Javascript and its my first fullstack project and it has a full feature API with json-web-token
+    authentication and sending Email and reset password and forgot password functionality it also has the Stripe in it 
+    `,
+    githubLink: "https://github.com/hmdrzaa11/Natours-React",
+    image: "natours.png",
+    stack: ["Node", "Express", "MongoDB", "Mongoose", "React", "Redux", "Jwt"],
+  },
 ];
 export default projects;
