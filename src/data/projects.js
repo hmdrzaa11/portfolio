5f8be8dd0db2533b91c511d321242e4d01a45252
Hamid Reza <hmdrzaa11@gmail.com>
2021-02-01 23:50:11 +0330

    added perfload app

--- src/data/projects.js
+++ src/data/projects.js
@@ -23,7 +23,7 @@ let projects = [
   {
     name: "Natours",
     description: `
-    This is the Movie "Natours" project written in Javascript and its my first fullstack project and it has a full feature API with json-web-token
+    This is the "Natours" project written in Javascript and its my first fullstack project and it has a full feature API with json-web-token
     authentication and sending Email and reset password and forgot password functionality it also has the Stripe in it 
     `,
     githubLink: "https://github.com/hmdrzaa11/Natours-React",
@@ -33,7 +33,7 @@ let projects = [
   {
     name: "Chat",
     description: `
-    This is the Movie "Chat" project written in Javascript and express and socket.io and react and its going to ask you a username and then 
+    This is the "Chat" project written in Javascript and express and socket.io and react and its going to ask you a username and then 
     redirects you to the "Namespaces" and provided from the server and each of them contains some rooms and then you click on a room and go to it
     and it has the room total dynamically change you can go to other rooms and come back and also keep a history of each room for new users that 
     join.
@@ -48,6 +48,27 @@ let projects = [
       "React",
       "Redux",
       "Styled-Components",
+    ],
+  },
+  {
+    name: "Performance Load",
+    description: `
+    This is the "Performance Load" project written in Javascript and socket.io and its Real time app and uses a client socket on the server to 
+    monitor the cpu and memory and show them on a canvas in the front-end as realtime and its scalable app and uses the redis server do that
+    and switch to long polling ajax request to be able to work with it you need to go to "client" , "nodeClient","server" and "npm install" 
+    and you need to have "redis" and "mongodb" running in background and its not responsive.
+    `,
+    githubLink: "https://github.com/hmdrzaa11/perfload",
+    image: "perfload.png",
+    stack: [
+      "Node",
+      "Express",
+      "Socket.io",
+      "Websocket",
+      "React",
+      "Mongoose",
+      "MongoDB",
+      "Redis",
     ],
   },
 ];
