049627067341fa757d864a24d21f236c149dfc68
Hamid Reza <hmdrzaa11@gmail.com>
2021-02-01 21:03:28 +0330

    added chat app

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,14 +8,14 @@ let projects = [
   },
   {
     name: "Maze",
-    description: `This is the Maze game which written in Vanilla js and i kept it very simple and its only in dark version`,
+    description: `This is the "Maze" game which written in Vanilla js and i kept it very simple and its only in dark version`,
     githubLink: "https://github.com/hmdrzaa11/maze",
     image: "maze.png",
     stack: ["Javascript", "Matter.js"],
   },
   {
     name: "Movie Fight",
-    description: `This is the Movie Fight and written in Vanilla js and fetches data from an API  and compares 2 movies side by side based on the IMDB and awards that each won etc.`,
+    description: `This is the "Movie Fight" and written in Vanilla js and fetches data from an API  and compares 2 movies side by side based on the IMDB and awards that each won etc.`,
     githubLink: "https://github.com/hmdrzaa11/movie-fight",
     image: "movieFight.png",
     stack: ["Javascript"],
@@ -23,12 +23,32 @@ let projects = [
   {
     name: "Natours",
     description: `
-    This is the Movie Natours project written in Javascript and its my first fullstack project and it has a full feature API with json-web-token
+    This is the Movie "Natours" project written in Javascript and its my first fullstack project and it has a full feature API with json-web-token
     authentication and sending Email and reset password and forgot password functionality it also has the Stripe in it 
     `,
     githubLink: "https://github.com/hmdrzaa11/Natours-React",
     image: "natours.png",
     stack: ["Node", "Express", "MongoDB", "Mongoose", "React", "Redux", "Jwt"],
+  },
+  {
+    name: "Chat",
+    description: `
+    This is the Movie "Chat" project written in Javascript and express and socket.io and react and its going to ask you a username and then 
+    redirects you to the "Namespaces" and provided from the server and each of them contains some rooms and then you click on a room and go to it
+    and it has the room total dynamically change you can go to other rooms and come back and also keep a history of each room for new users that 
+    join.
+    `,
+    githubLink: "https://github.com/hmdrzaa11/chat-app-react",
+    image: "chat.png",
+    stack: [
+      "Node",
+      "Express",
+      "Socket.io",
+      "Websocket",
+      "React",
+      "Redux",
+      "Styled-Components",
+    ],
   },
 ];
 export default projects;
